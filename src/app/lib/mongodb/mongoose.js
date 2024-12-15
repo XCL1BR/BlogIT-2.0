2ce88caf6ca3dc25225a67ba0e5e92baf0596d81
XCL1BR <sinha.ashwarya99@email.com>
2024-12-16 01:28:00 +0530

    Implement user management via webhooks: add user creation, update, and deletion logic; introduce MongoDB connection and user/post models.

---
+++ src/app/lib/mongodb/mongoose.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+let initialized = false;
+
+export const connect = async () => {
+    mongoose.set('strictQuery', true);
+    if(initialized) {
+        console.log('Already Connect To MongoDB');
+        return;
+    }
+    try {
+       await mongoose.connect(process.env.MONGODB_URI,{
+        dbName:'next-blog',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        
+       });
+       console.log('Connected to MongoDB');
+       initialized = true;
+    } catch (error) {
+         console.log('Error Connecting to MongoDB',error);
+         
+    }
+
+}
